d2972f802cef524619a68a18acf337d3ed579edf
Ashis Dhala Samanta <121971683+Regalflit0042@users.noreply.github.com>
2024-01-27 23:53:58 +0530

    Update app.py
    Added some words

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!, This is a basic python flask app'
+    return 'Hello, world!, This is a basic python flask app for the AWS CI/CD Pipeline.'
 
 if __name__ == '__main__':
     app.run()
