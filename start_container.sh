c09f94dfd979e9b96eaf02b8e211726fed7d5e26
Ashis Dhala Samanta <121971683+Regalflit0042@users.noreply.github.com>
2024-01-29 00:23:46 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull Docker image from Docker Hub
-echo
+docker pull regalflit0042/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 regalflit0042/simple-python-flask-app
+
