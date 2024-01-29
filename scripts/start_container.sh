59f5a573dda315616091022e2b7a6f67a34a5f4b
Ashis Dhala Samanta <121971683+Regalflit0042@users.noreply.github.com>
2024-01-30 00:20:11 +0530

    added the stop and start_container.sh to script folder

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull Docker image from Docker Hub
+docker pull regalflit0042/simple-python-flask-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 regalflit0042/simple-python-flask-app
